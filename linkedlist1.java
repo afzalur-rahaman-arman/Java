214ce00d2f691c2fa3fe9ce85df3c5406ed3cf38
Afzalur Rahaman Arman <52696103+afzalur-rahaman-arman@users.noreply.github.com>
2019-08-22 15:37:15 +0600

    Add files via upload

---
+++ linkedlist1.java
@@ -0,0 +1,35 @@
+package linkedlist_1;
+import java.util.*;
+public class linkedlist1 {
+
+  public static void main(String[] args) {
+
+        LinkedList<Integer> object = new LinkedList<Integer>(); 
+
+        object.add(88888); 
+
+        object.add(999); 
+
+        object.addLast(90); 
+
+        object.addFirst(77); 
+
+        object.add(2, 5); 
+
+        object.add(60); 
+
+        object.add(80); 
+
+        System.out.println("Linked list : " + object); 
+
+        object.remove(4); 
+
+        object.remove(3); 
+
+        object.removeFirst(); 
+
+        object.removeLast(); 
+
+        System.out.println("Linked list after deletion: " + object); 
+  }
+}
