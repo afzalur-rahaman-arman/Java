6b5d06880917e890adec81e24d7fce411fc3ebd0
Afzalur Rahaman Arman <52696103+afzalur-rahaman-arman@users.noreply.github.com>
2019-08-30 22:53:51 +0600

    Create One dimensional Array-2.java

---
+++ One dimensional Array-2.java
@@ -0,0 +1,15 @@
+//Array's improved version
+
+class AutoArray {
+ public static void main(String args[]) {
+ 
+int month_days[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 
+ 30, 31 };
+
+ System.out.println("April has " + month_days[3] + " days.");
+System.out.println("January has " + month_days[0] + " days.");
+System.out.println("February has " + month_days[1] + " days.");
+System.out.println("September has " + month_days[9] + " days.");
+ 
+}
+}
