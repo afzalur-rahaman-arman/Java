96ad03203ef8f7ee404f7ea7abb061700040890e
Afzalur Rahaman Arman <52696103+afzalur-rahaman-arman@users.noreply.github.com>
2019-09-04 23:46:36 +0600

    Java's inner class
    It's an inner class method in java.....

--- Inner class method.java
+++ Inner class method.java
@@ -1,11 +1,16 @@
 // Define an inner class within a for loop.
+
 class Outer {
+
  int outer_x = 100;
+
  void test() {
  for(int i=0; i<10; i++) {
+
  class Inner {
  void display() {
  System.out.println("display: outer_x = " + outer_x);
+
  }
  }
  Inner inner = new Inner();
@@ -13,9 +18,12 @@ class Outer {
  }
  }
 }
+
 class InnerClassDemo {
+
  public static void main(String args[]) {
  Outer outer = new Outer();
  outer.test();
+
  }
 }
